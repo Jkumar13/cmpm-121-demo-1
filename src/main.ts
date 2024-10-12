b8ee3b6942f0f3ffa763a4f9e7e9411abc181509
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2024-10-11 19:00:21 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,17 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement('button');
-button.textContent = '🔥Click this button🔥';
-document.body.appendChild(button);
+const button = document.createElement("button");
+button.textContent = "🔥Click to increase fire";
+app.appendChild(button);
+
+let sparks = 0;
+
+const counterText = document.createElement("counterText")
+counterText.textContent = "🔥Fire: " + sparks;
+app.appendChild(counterText);
+
+button.addEventListener('click', () => {
+    sparks += 1;
+    counterText.textContent = "🔥Fire: " + sparks;
+});
