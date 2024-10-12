3f2c57b73b748be24664b6b280c08d663d0eca73
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2024-10-11 18:29:22 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement('button');
+button.textContent = '🔥Click this button🔥';
+document.body.appendChild(button);
