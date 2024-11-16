b2746622d2baabfd0e6d2d7ed39bb5955a0bada2
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2024-11-15 17:29:13 -0800

    step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -11,43 +11,71 @@ app.append(header);
 
 let isPurchased = 0;
 
-const button = document.createElement("button");
-button.textContent = "Unlock the FIRE FACTORY (Costs 10 Fire)";
-app.appendChild(button);
+const Ten = document.createElement("button");
+Ten.textContent = "+0.1 Growth (Costs 10🔥)";
+app.appendChild(Ten);
 
-const clickerButton = document.createElement("button");
-clickerButton.textContent = "🔥CLICK to gain FIRE!";
-app.appendChild(clickerButton);
+const Hundred = document.createElement("button");
+Hundred.textContent = "+2.0 Growth (Costs 100🔥)";
+app.appendChild(Hundred);
 
-let sparks = 0;
+const Thousand = document.createElement("button");
+Thousand.textContent = "+50 Growth (Costs 1000🔥)";
+app.appendChild(Thousand);
+
+let sparks = 10;
 
 const counterText = document.createElement("counterText");
 counterText.textContent = "🔥Fire: " + sparks;
 app.appendChild(counterText);
 
 function incrementCounterSimple() {
-    sparks += isPurchased;
-    counterText.textContent = "🔥Fire: " + sparks;
-  }
+  sparks += isPurchased;
+  counterText.textContent = "🔥Fire: " + sparks;
+}
 
 setInterval(incrementCounterSimple, 1000);
 
 function incrementCounter(int: number) {
   sparks += int;
-  counterText.textContent = "🔥Fire: " + sparks.toFixed(0);
+  counterText.textContent = "🔥Fire: " + sparks.toFixed(0) + ", Growth: " + isPurchased.toFixed(1);
 }
-
-clickerButton.addEventListener("click", () => {
-    incrementCounter(1);
-    counterText.textContent = "🔥Fire: " + sparks;
+// clickerButton.addEventListener("click", () => {
+//   incrementCounter(1);
+//   counterText.textContent = "🔥Fire: " + sparks;
+// });
+let tenTrack = 0;
+let hundredTrack = 0;
+let thousandTrack = 0;
+Ten.addEventListener("click", () => {
+  if (sparks >= 10) {
+    sparks -= 10;
+    isPurchased += 0.1;
+    tenTrack += 1;
+    Ten.textContent =
+      "+0.1 Growth (Costs 10🔥, " + tenTrack + " Purchased)";
+  }
+  counterText.textContent = "🔥Fire: " + sparks;
 });
-button.addEventListener("click", () => {
-    if (sparks >= 10) {
-      sparks -= 10;
-      isPurchased += 1;
-      button.textContent = "🔥Spend 10 fire to POWER the factory (Growth Rate: " + isPurchased + ")";
-    }
-    counterText.textContent = "🔥Fire: " + sparks;
+Hundred.addEventListener("click", () => {
+  if (sparks >= 100) {
+    sparks -= 100;
+    isPurchased += 2;
+    hundredTrack += 1;
+    Hundred.textContent =
+      "+2 Growth (Costs 10🔥, " + hundredTrack + " Purchased)";
+  }
+  counterText.textContent = "🔥Fire: " + sparks;
+});
+Thousand.addEventListener("click", () => {
+  if (sparks >= 1000) {
+    sparks -= 1000;
+    isPurchased += 50;
+    thousandTrack += 1;
+    Thousand.textContent =
+      "+50 Growth (Costs 10🔥, " + thousandTrack + " Purchased)";
+  }
+  counterText.textContent = "🔥Fire: " + sparks;
 });
 requestAnimationFrame(animate);
 
@@ -58,11 +86,10 @@ function animate(currentTime: number) {
   if (previousTime == 0) {
     previousTime = currentTime;
   }
-  const seconds = (currentTime - previousTime) / (1000);
+  const seconds = (currentTime - previousTime) / 1000;
   exponentialCounter += seconds * isPurchased;
   console.log(exponentialCounter.toFixed(0));
   incrementCounter(exponentialCounter / 100);
   previousTime = currentTime;
   requestAnimationFrame(animate);
 }
-
